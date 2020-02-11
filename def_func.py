ecb3ea5926db47a0f5229f156df6bee2bb0d4751
王若水 <wang_ruoshui@outlook.com>
2020-02-11 20:06:00 +0800

    完成汉诺塔的移动

--- def_func.py
+++ def_func.py
@@ -14,3 +14,12 @@ def quadratic(a, b, c):
 
     return x1, x2
 
+
+# 汉诺塔的移动
+def move(n, a, b, c):
+    if n==1:
+        print(a, '-->', c)
+    else:
+        move(n-1, a, c, b)
+        move(1, a, b, c)
+        move(n-1, b, a, c)
