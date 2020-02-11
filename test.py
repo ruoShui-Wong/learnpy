2dba490b244d8bd2d030b5c82976a5fbb3887375
王若水 <wang_ruoshui@outlook.com>
2020-02-11 17:16:37 +0800

    建立了测试文件

---
+++ test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2020-02-11 17:15:39
+# @Author  : 王若水 (wang_ruoshui@outlook.com)
+# @Link    : http://example.org
+# @Version : $Id$
+
+def fcnAbs(x):
+    if x>=0:
+        return x
+    else:
+        return -x
+
