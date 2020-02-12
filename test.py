32cac52702292b6346d3ea8172e4819c448d681e
王若水 <wang_ruoshui@outlook.com>
2020-02-12 20:55:46 +0800

    装饰器示例代码

--- test.py
+++ test.py
@@ -5,12 +5,34 @@
 # @Link    : http://example.org
 # @Version : $Id$
 
-from HSSBC import createCounter
+from functools import wraps
 
-counterA = createCounter()
-print(counterA(), counterA(), counterA(), counterA(), counterA()) # 1 2 3 4 5
-counterB = createCounter()
-if [counterB(), counterB(), counterB(), counterB()] == [1, 2, 3, 4]:
-    print('测试通过!')
-else:
-    print('测试失败!')
+def log0(func):
+    @wraps(func)
+    def wrapper(*args, **kw):
+        print('call %s():' % func.__name__)
+        return func(*args, **kw)
+    return wrapper
+
+@log0
+def now():
+    print('2015-3-25')
+
+now()
+print(now.__name__)
+
+def log(text):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kw):
+            print('%s %s():' % (text, func.__name__))
+            return func(*args, **kw)
+        return wrapper
+    return decorator
+
+@log('execute')
+def now():
+    print('2015-3-25')
+
+now()
+print(now.__name__)
