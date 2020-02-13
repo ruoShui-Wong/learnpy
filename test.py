dd9e129eac2a9b97aeb7500844206592609559c0
王若水 <wang_ruoshui@outlook.com>
2020-02-13 16:46:17 +0800

    高级编程：@property

--- test.py
+++ test.py
@@ -5,26 +5,34 @@
 # @Link    : http://example.org
 # @Version : $Id$
 
-class Student(object):
-    count = 0
+class Screen(object):
+    @property
+    def width(self):
+        return self._width
 
-    def __init__(self, name):
-        self.name = name
-        
-        Student.count += 1
+    @width.setter
+    def width(self, width):
+        self._width = width
+    
+    @property
+    def height(self):
+        return self._height
+
+    @height.setter
+    def height(self, height):
+        self._height = height
+
+
+    @property
+    def resolution(self):
+        return self._height * self._width
 
 # 测试:
-if Student.count != 0:
-    print('测试失败!')
+s = Screen()
+s.width = 1024
+s.height = 768
+print('resolution =', s.resolution)
+if s.resolution == 786432:
+    print('测试通过!')
 else:
-    bart = Student('Bart')
-    if Student.count != 1:
-        print('测试失败!')
-    else:
-        lisa = Student('Bart')
-        if Student.count != 2:
-            print('测试失败!')
-        else:
-            print('Students:', Student.count)
-            print('测试通过!')
-
+    print('测试失败!')
