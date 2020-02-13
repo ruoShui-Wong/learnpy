04d9a933df8b5e4cab05e5d8ab779632a62e33e8
王若水 <wang_ruoshui@outlook.com>
2020-02-13 16:21:33 +0800

    实例属性和类属性学习

--- test.py
+++ test.py
@@ -5,9 +5,26 @@
 # @Link    : http://example.org
 # @Version : $Id$
 
-class MyObject(object):
-    def __init__(self):
-        self.x = 9
-    def power(self):
-        return self.x * self.x
+class Student(object):
+    count = 0
+
+    def __init__(self, name):
+        self.name = name
+        
+        Student.count += 1
+
+# 测试:
+if Student.count != 0:
+    print('测试失败!')
+else:
+    bart = Student('Bart')
+    if Student.count != 1:
+        print('测试失败!')
+    else:
+        lisa = Student('Bart')
+        if Student.count != 2:
+            print('测试失败!')
+        else:
+            print('Students:', Student.count)
+            print('测试通过!')
 
