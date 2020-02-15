31fc4ce34aa94a74785fdf5608f2555b74a19890
王若水 <wang_ruoshui@outlook.com>
2020-02-15 15:58:58 +0800

    IO编程：序列化

--- test.py
+++ test.py
@@ -5,6 +5,25 @@
 # @Link    : http://example.org
 # @Version : $Id$
 
-from IO.dirIO import findFille
+import json
 
-findFille('O', 'IO')
+class Student(object):
+    def __init__(self, name, age, score):
+        self.name = name
+        self.age = age
+        self.score = score
+
+def student2dict(std):
+    return {
+        'name': std.name,
+        'age': std.age,
+        'score': std.score
+    }
+def dict2student(d):
+    return Student(d['name'], d['age'], d['score'])
+
+s = Student('Bob', 20, 88)
+print(json.dumps(s, default=lambda obj: obj.__dict__))
+
+json_str = '{"age": 20, "score": 88, "name": "Bob"}'
+print(json.loads(json_str, object_hook=dict2student))
